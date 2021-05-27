cf14240603de1311e062060941d3ed010657d0cd
Laiane Ribeiro <laiane.ribeiro@sankhya.com.br>
2021-05-27 12:25:42 -0300

    Importando exercicio teste

---
+++ src/Exercicios_M30.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+ import java.util.Scanner;
+
+public class Exercicios_M30 {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+     	Scanner sc = new Scanner(System.in);
+		int x, y, soma;
+		x = sc.nextInt();
+		y = sc.nextInt();
+		soma = x + y;
+
+		System.out.println("SOMA = " + soma);
+
+		sc.close();
+ 
+    }
+ 
+}
